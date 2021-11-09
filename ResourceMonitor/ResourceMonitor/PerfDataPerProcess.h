4e88cd336acb24853e9680619e32d325738fa1ec
tasddc1226 <tasddc@naver.com>
2021-11-09 16:36:09 +0900

    [add] 쿼리를 통해 얻어온 값을 각 프로세스 별 table에 저장하는 부분

---
+++ ResourceMonitor/ResourceMonitor/PerfDataPerProcess.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "PerfData.h"
+
+#define PROCESS_ID_INDEX 0
+#define PROCESS_NAME_INDEX 1
+#define PROCESS_WORKINGSET_INDEX 2
+#define PROCESS_USEAGERATE_INDEX 3
+#define PROCESS_IOREAD_INDEX 4
+#define PROCESS_IOWRITE_INDEX 5
+
+struct PerProcessDataObj : public DataObj
+{
+	CString name;
+	CString workingSet;
+	CString usageRate;
+	CString ioRead;
+	CString ioWrite;
+	virtual void Clear()
+	{
+		name.Empty();
+		workingSet.Empty();
+		usageRate.Empty();
+		ioRead.Empty();
+		ioWrite.Empty();
+	}
+};
+
+class CPerfDataPerProcess :
+	public CPerfData
+{
+public:
+	CPerfDataPerProcess();
+	~CPerfDataPerProcess();
+	int GetNumberOfCores();
+public:
+
+	int nCores;
+	vector<ULONGLONG> exitedProcIDs;
+	double usingPercent;
+	double idlePercent;
+	map<ULONGLONG, PerProcessDataObj>	*m_table;
+
+	virtual void Init(const PerfDataInfo& info, CResourceMonitorDoc* doc);
+	virtual void GetData();
+};
+
