beea532d9e2a1286c729d26d9753f310d3049448
tasddc1226 <tasddc@naver.com>
2021-11-09 16:31:56 +0900

    [chore] setup file 추가

---
+++ ResourceMonitor/ResourceMonitor/Settings.h
@@ -0,0 +1,7 @@
+#pragma once
+#define VERSION "v1.0.1"
+#define FRAME_WIDTH 60
+#define UPDATE_INTERVAL 1000
+#define LOG_INTERVAL 10000
+#define LOG_DIRECTORY _T("D:\\Performance_data_Log")
+#define LOG_TOP_N 10
