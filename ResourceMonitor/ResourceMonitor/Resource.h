ac27fcd3df8350a6bef970b2ace1c24350a414a3
tasddc1226 <tasddc@naver.com>
2021-11-11 14:21:06 +0900

    [fix] Setting, Resource 헤더 수정

--- ResourceMonitor/ResourceMonitor/Resource.h
+++ ResourceMonitor/ResourceMonitor/Resource.h
@@ -1,18 +1,26 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// ResourceMonitor.rc에서 사용됩니다.
+// ResourceMonitor.rc에서 사용되고 있습니다.
 //
-#define IDD_ABOUTBOX				100
-#define IDR_MAINFRAME				128
-#define IDR_ResourceMonitorTYPE				130
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_ResourceMonitorTYPE         130
+#define IDD_SET_LOG_INTERVAL            310
+#define IDD_SET_THRESHOLD               312
+#define IDC_EDIT1                       1000
+#define IDC_EDIT_MEM                    1001
+#define IDC_EDIT_CPU                    1002
+#define ID_32771                        32771
+#define ID_SET_LOG_INTERVAL             32772
+#define ID_LOG_SETTHRESHOLD             32773
 
-// 다음은 새 개체에 사용할 기본값입니다.
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_RESOURCE_VALUE	314
+#define _APS_NEXT_CONTROL_VALUE		1003
 #define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_COMMAND_VALUE		32774
 #endif
 #endif
--- ResourceMonitor/ResourceMonitor/Settings.h
+++ ResourceMonitor/ResourceMonitor/Settings.h
@@ -1,7 +1,16 @@
 #pragma once
-#define VERSION "v1.0.1"
+#define VERSION "v1.1.0"
+
 #define FRAME_WIDTH 60
 #define UPDATE_INTERVAL 1000
-#define LOG_INTERVAL 10000
+
+
+//#define LOG_INTERVAL 1000
+
+#define LOG_INTERVAL 1000
+
 #define LOG_DIRECTORY _T("D:\\Performance_data_Log")
+#define LOG_CPU_THRESHOLD 10.0f
+#define LOG_MEM_THRESHOLD 1048576 //1GB
+
 #define LOG_TOP_N 10
